9fe435386d68b2492a6b8415cc80007250554a1e
ClairBee <clair.with.no.e@gmail.com>
2016-11-15 17:12:27 +0000

    Errors & RMSEs calculated for raw forecasts

--- Code/01-import-and-processing.R
+++ Code/01-import-and-processing.R
@@ -1,11 +1,18 @@
 
 library("CB.Misc"); library("SX.weather")
 
-fc <- readRDS("./Data/Forecasts.rds")
+load.all()
 
 ####################################################################################################
 
-# LOAD ALL DATA INTO SINGLE ARRAY FOR EASIER MANIPULATION                                       ####
+# NOTES                                                                                         ####
+
+# checked alignment of forecast & obs visually - data appear to be aligned (days 16:105)
+
+
+####################################################################################################
+
+# LOAD ALL FORECASTS INTO SINGLE ARRAY FOR EASIER MANIPULATION                                  ####
 
 # create single array: var . day . year . leadtime . version (0 = control)
 fc <- array(dim = c(5, 105, 7, 15, 51),
@@ -61,3 +68,134 @@ saveRDS(fc, "./Data/Forecasts.rds")
 
 ####################################################################################################
 
+# LOAD ALL OBSERVATIONS INTO SINGLE ARRAY                                                       ####
+
+obs <- array(dim = c(5, 90, 7), dimnames = list(dimnames(fc)[[1]], c(1:90), dimnames(fc)[[3]]))
+
+# principal components of mean sea level pressure
+{
+    obs.mslp <- load.data("./Data/ERAint_pca_timeseries.rda")
+    
+    # 3240 - 720 + 1 = 2611, this is the start time for 2007/2008-2013/2014.
+    obs["pc1",,] <- obs.mslp[2521:3150,1] 
+    obs["pc2",,] <- obs.mslp[2521:3150,2] 
+    obs["pc3",,] <- obs.mslp[2521:3150,3] 
+}
+
+# temperatures
+{
+    # didn't investigate indexing for this file - this was provided by SX
+    # take every 4th, starting from 3rd (midday measurement?); 29:35 gives 7 years' data
+    obs["temp.n",,] <- load.data("./Data/ERAint_temp2m_36yr_north24hrave.rda")[seq(3,360,4), 29:35] - 273.15
+    obs["temp.s",,] <- load.data("./Data/ERAint_temp2m_36yr_south24hrave.rda")[seq(3,360,4), 29:35] - 273.15
+    
+}
+
+# check that these are properly aligned with forecasts
+{
+    par(mfrow = c(5, 7), mar = c(0,0,0,0), lwd = 3, oma = c(0,0,3,0))
+    
+    invisible(sapply(1:5, function(varbl) {
+        invisible(sapply(formatC(8:14, width = 2, flag = "0"), function(yr) {
+            plot(obs[varbl,,yr], type = "l", xlab = "", ylab = "", yaxt = "none", xaxt = "none", lwd = 1)
+            lines(fc[varbl,16:105,yr,"0","c"], col = "red", lwd = 1)
+        }))
+    }))
+    mtext("Check alignment of control forecast (red) & observations (black)", outer = TRUE)
+}
+# control forecast from days 16:105 seems to be matched to observations. Assume dates properly aligned.
+
+saveRDS(obs, "./Data/Observations.rds")
+
+####################################################################################################
+
+# CALCULATE MEAN ERROR, RMSE                                                                    ####
+
+####################################################################################################
+
+ens.mean <- apply(fc[,,,,2:51], c(1,2,3,4), mean)
+
+ens.mean.error <- apply(sweep(ens.mean, 1:3, obs, "-"), c(1, 4), mean)
+ctrl.mean.error <- apply(sweep(fc[,,,,1], 1:3, obs, "-"), c(1, 4), mean)
+
+suppressWarnings(ens.member.errors <- apply(sweep(fc[,,,,2:51], c(1:3, 5), obs, "-"), c(1, 4, 5), mean))
+
+# plot forecast errors for all variables - compare to figure 5.1 in dissertation
+    # plots are similar, but not identical (particularly after 10 days?)
+    # need to get date reference file in order to investigate further.
+{
+    # quick plotting function
+    qplot <- function(element, ...) {
+        
+        ttl <- switch(element,
+                      "temp.s" = "Temp (south)", "temp.n" = "Temp (north)",
+                      "pc1" = "First PC", "pc2" = "Second PC", "pc3" = "Third PC")
+        
+        matplot(ens.member.errors[element,,], type = "l", col = adjustcolor("grey", alpha = 0.4),
+                lty = 1, main = ttl, xlab = "", ylab = "", ...)
+        
+        lines(ens.mean.error[element,])
+        lines(ctrl.mean.error[element,], col = "blue3")
+    }
+    
+    par(mfrow = c(2,3), oma = c(0.5, 0.5, 2, 0.5), mar = c(2,2,3,1))
+    qplot("pc1"); qplot("pc2"); qplot("pc3")
+    qplot("temp.n"); qplot("temp.s")
+    
+    plot.new()
+    legend("left", lty = 1, col = c("blue", "black", adjustcolor("grey", alpha = 0.5)), bty = "n", cex = 1.1,
+           legend = c("Control forecast", "Perturbed mean", "Perturbed members"))
+    
+    mtext("Mean error at each forecast lead time", outer = TRUE, cex = 1)
+}
+
+ctrl.rmse <- apply(sweep(fc[,,,,1], 1:3, obs, "-"), c(1, 4), function(err) sqrt(mean(err^2)))
+ens.rmse <- apply(sweep(ens.mean, 1:3, obs, "-"), c(1, 4), function(err) sqrt(mean(err^2)))
+suppressWarnings(ens.member.rmse <- apply(sweep(fc[,,,,2:51], c(1:3, 5), obs, "-"), c(1, 4, 5), function(err) sqrt(mean(err^2))))
+
+# plot forecast rmse for all variables - compare to figure 5.2 in dissertation
+    # 
+{
+    # quick plotting function
+    qplot <- function(element, ...) {
+        
+        ttl <- switch(element,
+                      "temp.s" = "Temp (south)", "temp.n" = "Temp (north)",
+                      "pc1" = "First PC", "pc2" = "Second PC", "pc3" = "Third PC")
+        
+        matplot(ens.member.rmse[element,,], type = "l", col = adjustcolor("grey", alpha = 0.4),
+                lty = 1, main = ttl, xlab = "", ylab = "", ...)
+        
+        lines(ens.rmse[element,])
+        lines(ctrl.rmse[element,], col = "blue3")
+    }
+    
+    par(mfrow = c(2,3), oma = c(0.5, 0.5, 2, 0.5), mar = c(2,2,3,1))
+    qplot("pc1"); qplot("pc2"); qplot("pc3")
+    qplot("temp.n"); qplot("temp.s")
+    
+    plot.new()
+    legend("left", lty = 1, col = c("blue", "black", adjustcolor("grey", alpha = 0.5)), bty = "n", cex = 1.1,
+           legend = c("Control forecast", "Perturbed mean", "Perturbed members"))
+    
+    mtext("RMSE at each forecast lead time", outer = TRUE, cex = 1)
+}
+
+####################################################################################################
+
+# CASE STUDY: CONTROL FORECAST FOR TEMP (NORTH), 2008                                           ####
+
+plot(obs["temp.n",,"08"], type = "l", lwd = 2, ylab = "", xlab = "", main = "Obs vs control forecast")
+lines(fc["temp.n", , "08", "0", "c"], col = "blue")
+
+error <- fc["temp.n", , , "0", "c"] - obs["temp.n",,]
+
+matplot(error, type = "l", lty = 1, main = "Same-day control forecast error")
+apply(error, 2, mean)
+mean(error)
+ctrl.mean.error["temp.n", "0"]
+
+sqrt(mean(error^2))
+ctrl.rmse["temp.n", "0"]
+
+# same result. Don't think this is a problem with my calculations.
--- Code/Sichun-code-replicated.R
+++ Code/Sichun-code-replicated.R
@@ -17,6 +17,7 @@ library("CB.Misc"); library("SX.weather")
     # ECMWF_europe_lon.rda
     # ECMWF_europe_lat.rda
     # ECMWFctrl_mslp_8yr.rda
+    # ERAint_europe_starttime.rda
 
 # Sichun code used, in order:
     # ECMWF_loading.R
@@ -116,4 +117,169 @@ pert_obs.all <- data.frame(year = rep(formatC(c(8:14), width = 2, flag = "0"), e
 
 ####################################################################################################
 
+# ECMWF_Mean error and RMSE.E                                                                   ####
+
+# variable names retained, method of obtaining them corrected
+
+fc <- readRDS("./Data/Forecasts.rds")
+
+# The control forecasts for 14 lead times and 630 days (90 winter days * 7 years).
+PC1.ctrl.leadtime_ECMWF <- array(fc["pc1",16:105,,2:15,"c"], dim = c(90 * 7, 14))
+PC2.ctrl.leadtime_ECMWF <- array(fc["pc2",16:105,,2:15,"c"], dim = c(90 * 7, 14))
+PC3.ctrl.leadtime_ECMWF <- array(fc["pc3",16:105,,2:15,"c"], dim = c(90 * 7, 14))
+TN.ctrl.leadtime_ECMWF <- array(fc["temp.n",16:105,,2:15,"c"], dim = c(90 * 7, 14))
+TS.ctrl.leadtime_ECMWF <- array(fc["temp.s",16:105,,2:15,"c"], dim = c(90 * 7, 14))
+
+# The perturbed forecasts for 14 lead times and 630 days.
+PC1.pert.leadtime_ECMWF <- array(aperm(fc, c(1,2,3,5,4))["pc1",16:105,,2:51, 2:15], dim = c(90 * 7 * 50, 14))
+PC2.pert.leadtime_ECMWF <- array(aperm(fc, c(1,2,3,5,4))["pc2",16:105,,2:51, 2:15], dim = c(90 * 7 * 50, 14))
+PC3.pert.leadtime_ECMWF <- array(aperm(fc, c(1,2,3,5,4))["pc3",16:105,,2:51, 2:15], dim = c(90 * 7 * 50, 14))
+TN.pert.leadtime_ECMWF <- array(aperm(fc, c(1,2,3,5,4))["temp.n",16:105,,2:51, 2:15], dim = c(90 * 7 * 50, 14))
+TS.pert.leadtime_ECMWF <- array(aperm(fc, c(1,2,3,5,4))["temp.s",16:105,,2:51, 2:15], dim = c(90 * 7 * 50, 14))
+
+
+#===========================================================================================
+# observations from ERA-INT
+
+timeseries <- load.data("./Data/ERAint_pca_timeseries.rda")
+
+# 3240 - 720 + 1 = 2611, this is the start time for 2007/2008-2013/2014.
+PC1_obs <- timeseries[2521:3150,1] 
+PC2_obs <- timeseries[2521:3150,2] 
+PC3_obs <- timeseries[2521:3150,3] 
+
+# didn't investigate indexing for this file - this was provided by SX
+TN_obs <- c(load.data("./Data/ERAint_temp2m_36yr_north24hrave.rda")[seq(3,360,4),29:35]) - 273.15
+TS_obs <- c(load.data("./Data/ERAint_temp2m_36yr_south24hrave.rda")[seq(3,360,4),29:35]) - 273.15
+
+#===========================================================================================
+# mean error
+
+# for control forecast 
+{
+    # mean error = sum(PC1.ctrl.leadtime-PC1.ctrl_obs)/630
+    mean.error.ctrl_PC1_ECMWF <- colMeans(PC1.ctrl.leadtime_ECMWF - PC1_obs)
+    mean.error.ctrl_PC2_ECMWF <- colMeans(PC2.ctrl.leadtime_ECMWF - PC2_obs)
+    mean.error.ctrl_PC3_ECMWF <- colMeans(PC3.ctrl.leadtime_ECMWF - PC3_obs)
+    mean.error.ctrl_TN_ECMWF <- colMeans(TN.ctrl.leadtime_ECMWF - TN_obs)
+    mean.error.ctrl_TS_ECMWF <- colMeans(TS.ctrl.leadtime_ECMWF - TS_obs)
+}
+
+# for perturbed forecast
+{
+    # first get the ensemble mean for each variable and each lead time
+    PC1.pert.leadtime.ensemblemean_ECMWF <- apply(array(PC1.pert.leadtime_ECMWF, dim = c(630, 50, 14)), c(1,3), mean)
+    PC2.pert.leadtime.ensemblemean_ECMWF <- apply(array(PC2.pert.leadtime_ECMWF, dim = c(630, 50, 14)), c(1,3), mean)
+    PC3.pert.leadtime.ensemblemean_ECMWF <- apply(array(PC3.pert.leadtime_ECMWF, dim = c(630, 50, 14)), c(1,3), mean)
+    TN.pert.leadtime.ensemblemean_ECMWF <- apply(array(TN.pert.leadtime_ECMWF, dim = c(630, 50, 14)), c(1,3), mean)
+    TS.pert.leadtime.ensemblemean_ECMWF <- apply(array(TS.pert.leadtime_ECMWF, dim = c(630, 50, 14)), c(1,3), mean)
+    
+    # then mean error
+    mean.error.pert_PC1_ECMWF <- colMeans(PC1.pert.leadtime.ensemblemean_ECMWF - PC1_obs)
+    mean.error.pert_PC2_ECMWF <- colMeans(PC2.pert.leadtime.ensemblemean_ECMWF - PC2_obs)
+    mean.error.pert_PC3_ECMWF <- colMeans(PC3.pert.leadtime.ensemblemean_ECMWF - PC3_obs)
+    mean.error.pert_TN_ECMWF <- colMeans(TN.pert.leadtime.ensemblemean_ECMWF - TN_obs)
+    mean.error.pert_TS_ECMWF <- colMeans(TS.pert.leadtime.ensemblemean_ECMWF - TS_obs)
+}
+
+# (my addition) mean error of each perturbation
+{
+    PC3.error.envelope <- array(PC3.pert.leadtime_ECMWF, dim = c(630, 50, 14)) - PC3_obs
+    PC2.error.envelope <- array(PC2.pert.leadtime_ECMWF, dim = c(630, 50, 14)) - PC2_obs
+    PC1.error.envelope <- array(PC1.pert.leadtime_ECMWF, dim = c(630, 50, 14)) - PC1_obs
+    TN.error.envelope <- array(TN.pert.leadtime_ECMWF, dim = c(630, 50, 14)) - TN_obs
+    TS.error.envelope <- array(TS.pert.leadtime_ECMWF, dim = c(630, 50, 14)) - TS_obs
+}
+
+# plots of mean error for each component, averaged over 630 days at each lead time
+{
+    qplot <- function(element, ...) {
+        
+        ttl <- switch(element,
+                      "TS" = "Temp (south)", "TN" = "Temp (north)",
+                      "PC1" = "First PC", "PC2" = "Second PC", "PC3" = "Third PC")
+        
+        matplot(apply(eval(parse(text = paste0(element, ".error.envelope"))), 3:2, mean),
+                type = "l", col = adjustcolor("grey", alpha = 0.4), lty = 1, main = ttl,
+                xlab = "", ylab = "", ...)
+        
+        lines(eval(parse(text = paste0("mean.error.ctrl_", element, "_ECMWF"))), col = "blue3")
+        lines(eval(parse(text = paste0("mean.error.pert_", element, "_ECMWF"))))
+    }
+    
+    par(mfrow = c(2,3), oma = c(0.5, 0.5, 2, 0.5), mar = c(2,2,3,1))
+    qplot("PC1"); qplot("PC2"); qplot("PC3")
+    qplot("TN"); qplot("TS")
+    
+    plot.new()
+    legend("left", lty = 1, col = c("blue", "black", adjustcolor("grey", alpha = 0.5)), bty = "n", cex = 1.1,
+           legend = c("Control forecast", "Perturbed mean", "Perturbed members"))
+    
+    mtext("Mean error at each forecast lead time", outer = TRUE, cex = 1.5)
+    
+    par(mfrow = c(1,1), oma = c(0,0,0,0), mar = c(5.1, 4.1, 4.1, 2.1))
+    
+    # fix same scales for easier comparison with dissertation
+    par(mfrow = c(2,2), oma = c(0.5, 0.5, 2, 0.5), mar = c(2,2,3,1))
+    qplot("TN", ylim = c(-2.3,-1.85)); qplot("TS", ylim = c(-1.1, -0.85)) 
+    qplot("PC1", ylim = c(-0.22,0)); qplot("PC2", ylim = c(0.1,0.25))
+
+}
+
+#===========================================================================================
+# root mean squared error
+
+# for control forecast
+# RMSE=sum(leadtime-observation)^2/630
+RMSE.ctrl_PC1_ECMWF <- sqrt(colMeans((PC1.ctrl.leadtime_ECMWF - PC1_obs)^2))
+RMSE.ctrl_PC2_ECMWF <- sqrt(colMeans((PC2.ctrl.leadtime_ECMWF - PC2_obs)^2))
+RMSE.ctrl_PC3_ECMWF <- sqrt(colMeans((PC3.ctrl.leadtime_ECMWF - PC3_obs)^2))
+RMSE.ctrl_TN_ECMWF<- sqrt(colMeans((TN.ctrl.leadtime_ECMWF - TN_obs)^2))
+RMSE.ctrl_TS_ECMWF <- sqrt(colMeans((TS.ctrl.leadtime_ECMWF - TS_obs)^2))
+
+# for perturbed forecast
+RMSE.pert_PC1_ECMWF <- sqrt(colMeans((PC1.pert.leadtime.ensemblemean_ECMWF - PC1_obs)^2))
+RMSE.pert_PC2_ECMWF <- sqrt(colMeans((PC2.pert.leadtime.ensemblemean_ECMWF - PC2_obs)^2))
+RMSE.pert_PC3_ECMWF <- sqrt(colMeans((PC3.pert.leadtime.ensemblemean_ECMWF - PC3_obs)^2))
+RMSE.pert_TN_ECMWF <- sqrt(colMeans((TN.pert.leadtime.ensemblemean_ECMWF - TN_obs)^2))
+RMSE.pert_TS_ECMWF <- sqrt(colMeans((TS.pert.leadtime.ensemblemean_ECMWF - TS_obs)^2))
+
+# Sichun's plots
+{
+    # Temperature in North UK
+    plot(c(1:14),RMSE.ctrl_TN_ECMWF,xlab='lead time(day)',ylab='Root Mean Square Error',type='l',col='red',main='(a) Temperature in North UK')
+    axis(1,at=c(1:14))
+    lines(c(1:14),RMSE.pert_TN_ECMWF,lty=2,col='blue')
+    abline(h=0,col="grey",lty=2,lwd=1)
+    legend(7,2.3,legend=c('Control Forecast','Perturbed Forecast'),col=c('red','blue'),
+           lty=c(1,2),cex = 0.7,bty = 'n')
+    
+    # Temperature in South UK
+    plot(c(1:14),RMSE.ctrl_TS_ECMWF,xlab='lead time(day)',ylab='Root Mean Square Error',type='l',col='red', main='(b) Temperature in South UK')
+    axis(1,at=c(1:14))
+    lines(c(1:14),RMSE.pert_TS_ECMWF,lty=2,col='blue')
+    abline(h=0,col="grey",lty=2,lwd=1)
+    legend(7,1.5,legend=c('Control Forecast','Perturbed Forecast'),col=c('red','blue'),
+           lty=c(1,2),cex = 0.7,bty = 'n')
+    
+    # PC1
+    plot(c(1:14),RMSE.ctrl_PC1_ECMWF,xlab='lead time(day)',ylab='Root Mean Square Error',type='l',col='red',main='(c) PC1 of Pressure',ylim = c(0,1.2))
+    axis(1,at=c(1:14))
+    lines(c(1:14),RMSE.pert_PC1_ECMWF,lty=2,col='blue')
+    abline(h=0,col="grey",lty=2,lwd=1)
+    legend(7,0.2,legend=c('Control Forecast','Perturbed Forecast'),col=c('red','blue'),
+           lty=c(1,2),cex = 0.7,bty = 'n')
+    
+    # PC2
+    plot(c(1:14),RMSE.ctrl_PC2_ECMWF,xlab='lead time(day)',ylab='Root Mean Square Error',type='l',col='red',main='(d) PC2 of Pressure',ylim = c(0,1.2))
+    axis(1,at=c(1:14))
+    lines(c(1:14),RMSE.pert_PC2_ECMWF,lty=2,col='blue')
+    abline(h=0,col="grey",lty=2,lwd=1)
+    legend(7,0.2,legend=c('Control Forecast','Perturbed Forecast'),col=c('red','blue'),
+           lty=c(1,2),cex = 0.7,bty = 'n')
+}
+
+####################################################################################################
+
+# ECMWF_RMSE vs spread.R                                                                        ####
 
